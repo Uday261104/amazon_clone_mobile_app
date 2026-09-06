4c8579545cf5ea04162fefbc703891228edb4996
Uday <uday.s@cron-labs.com>
2026-09-06 23:22:53 +0530

    added provider

---
+++ frontend/lib/controller/provider_controller/user_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+import '../../model/userModel.dart';
+
+class UserProvider extends ChangeNotifier {
+  User _user=User(id: '', email: '', name: '', password:'', address: '', type: '');
+
+  User get user=>_user;
+
+  void setUser(Map<String,dynamic> user){
+    _user=User.fromDB(user);
+
+    notifyListeners();
+  }
+
+}
